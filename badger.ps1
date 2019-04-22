37f6da26681532ec904683e7cad7430212eb38f4
jasonbuechler <34217071+jasonbuechler@users.noreply.github.com>
2019-04-22 11:58:05 -0700

    Create badger.ps1

---
+++ badger.ps1
@@ -0,0 +1,4 @@
+#https://www.bleepingcomputer.com/news/microsoft/heres-how-to-enable-the-built-in-windows-10-openssh-client/
+#https://man.openbsd.org/ssh
+#scp scp://USERNAME@HOST:PORT//FILEPATH/file.txt ./
+#ssh ssh://USERNAME@HOST:PORT

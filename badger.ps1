d4fa3d2561c4ec94f915a6534471a5cf5d1769fc
jasonbuechler <34217071+jasonbuechler@users.noreply.github.com>
2019-04-22 14:43:43 -0700

    Update badger.ps1

--- badger.ps1
+++ badger.ps1
@@ -2,3 +2,30 @@
 #https://man.openbsd.org/ssh
 #scp scp://USERNAME@HOST:PORT//FILEPATH/file.txt ./
 #ssh ssh://USERNAME@HOST:PORT
+
+echo Badger is on the move
+
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+$currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if($currentPrincipal -ne 'True'){ 
+	write-host 'you done bad. relaunch this as an admin'
+    Exit
+}
+
+write-host 'Enabling the TFTP "windows feature"'
+Enable-WindowsOptionalFeature -Online -FeatureName "TFTP" -all
+
+$tftpstate = (Get-WindowsOptionalFeature -FeatureName "TFTP" -online).State
+if($tftpstate -ne 'Enabled'){
+    write-host 'Couldnt enable TFTP feature'
+    Exit
+}
+
+$cnt = 0
+while($cnt -lt 4){
+    $ping = test-connection -ComputerName google.com -Quiet -Count 1
+    if ($ping){ $cnt++ }
+    else{ $cnt = 0 }
+    write-host $cnt' pings in-a-row, so far'
+}
+write-host 'hey wadda you know its been up: 4 in a row'

467be45fe8f55775875da909e4f25018c5d5b1fe
jasonbuechler <34217071+jasonbuechler@users.noreply.github.com>
2019-05-06 17:05:52 -0700

    completed cfe personalizer
    context strings also complete

--- bits/cfe personalizer.ps1
+++ bits/cfe personalizer.ps1
@@ -3,7 +3,6 @@
 $cfe     = 'original_cfe.bin'
 $cfe_new = 'new_cfe.bin'
 $cfe_bak = 'new_cfe.bin.bak'
-$cfe_unp = 'unpatched_new_cfe.bin'
 
 $macpatt = "(([0-9A-FX]{2}\:){5}[0-9A-FX]{2})" # note the X in there for "XX:XX:XX..." prepped cfe's
 $wpspatt = "([0-9X]{8})"
@@ -39,7 +38,7 @@ $keep4_val  = $keep4.Matches.Groups[1].Value
 $keep1_valB = [system.text.encoding]::ASCII.GetBytes($keep1_val) # byte values of the 1st mac in the cfe
 $keep2_valB = [system.text.encoding]::ASCII.GetBytes($keep2_val) # Needed both for sanity-check reads, and
 $keep3_valB = [system.text.encoding]::ASCII.GetBytes($keep3_val) # for overwriting bytes of target cfe
-$keep4_valB = [system.text.encoding]::ASCII.GetBytes($keep4_val) # 
+$keep4_valB = [system.text.encoding]::ASCII.GetBytes($keep4_val) # note this (WPS code) is only 8 bytes
 $kill1_addr = $kill1.Matches.Groups[1].Index
 $kill2_addr = $kill2.Matches.Groups[1].Index
 $kill3_addr = $kill3.Matches.Groups[1].Index
@@ -51,15 +50,20 @@ $kill4_val  = $kill4.Matches.Groups[1].Value
 $kill1_valB = [system.text.encoding]::ASCII.GetBytes($kill1_val)
 $kill2_valB = [system.text.encoding]::ASCII.GetBytes($kill2_val)
 $kill3_valB = [system.text.encoding]::ASCII.GetBytes($kill3_val)
-$kill4_valB = [system.text.encoding]::ASCII.GetBytes($kill4_val)
+$kill4_valB = [system.text.encoding]::ASCII.GetBytes($kill4_val) # note this (WPS code) is only 8 bytes
 
-# purely informational printing
+#
+# This next big chunk is purely for informational logging/notification.
+# A savvy user could use HxD to visit the stated addresses and verify.
+# Also, a visual sanity check for the user.
+# 
 # ex:   38:2C:4A:EF:3E:88 [1233 / 0x4d1] <-- et0macaddr=38:2C:4A:EF:3E:88
 #       |                  |                 |
 #   mac pattern            |                 |
 #              byte-0 decimal/hex address    |
 #                                    full matched pattern
 $reporting = @"
+
 original_cfe.bin
 * $keep1_val [$keep1_addr / $([String]::Format("0x{0:x3}", $keep1_addr))] <-- $($keep1.matches.value)
 * $keep2_val [$keep2_addr / $([String]::Format("0x{0:x3}", $keep2_addr))] <-- $($keep2.matches.value)
@@ -74,53 +78,65 @@ new_cfe.bin (before patching)
 
 "@
 write-host $reporting -foregroundColor yellow
-pause
 write-host ''
 
-
+#
+# SANITY CHECK #1
+#
 # open the CFE for read-checks
 # read into 17-byte array
-# (prob need to diff array for secret_code)
+# compare array with keep_val
+#
 $bytes = New-Object -TypeName Byte[] -ArgumentList 17
-
 $bytes.clear()								# zeroes all 17 elements of $bytes (unnecessary but nice)
 $fs = [IO.File]::OpenRead($cfe) 			# open original_cfe.bin as filestream "$fs" (for reading)
 $fs.Seek($keep3_addr,'Begin') | out-null	# set the "current" address to presumed address of keep3_val
 $fs.Read($bytes,0,17) | out-null 			# read 17 bytes from cfe into the 17-byte buffer, "$bytes"
 $fs.close()
-
 $bytes_as_string = [System.Text.Encoding]::ASCII.GetString($bytes)
 if( $bytes_as_string -eq $keep3_val ){
-    write-host -foregroundcolor green "1st read-address sanity check passed :)"
+    write-host -foregroundcolor green "1st (read-address) sanity check passed :)"
 }else{
-    write-host -foregroundcolor red "fail!"
+    write-host -foregroundcolor red "fail! bytes read from $cfe didn't match the expected mac-address pattern."
     pause
 }
 
 
 #
+# SANITY CHECK #2
+#
 # now we repeat the process to check address validity in the other (new) cfe filestream
 # (since it's a different origin, who knows, maybe it has different encoding)
 #
-
 $bytes.clear()								# zeroes all 17 elements of $bytes (good idea now)
 $fs = [IO.File]::OpenRead($cfe_new) 		# open new_cfe.bin as filestream "$fs" (for reading)
 $fs.Seek($kill3_addr,'Begin') | out-null	# set the "current" address to presumed address of kill3_val
 $fs.Read($bytes,0,17) | out-null 			# read 17 bytes from cfe into the 17-byte buffer, "$bytes"
 $fs.close()
-
 $bytes_as_string = [System.Text.Encoding]::ASCII.GetString($bytes)
 if( $bytes_as_string -eq $kill3_val ){
-    write-host -foregroundcolor green "2nd read-address sanity check passed :)"
+    write-host -foregroundcolor green "2nd (read-address) sanity check passed :)"
 }else{
-    write-host -foregroundcolor red "fail!"
+    write-host -foregroundcolor red "fail! bytes read from $cfe_new didn't match the expected mac-address pattern."
     pause
 }
 
+
+
+#
+# ACTUAL PERSONALIZATION OF new_cfe.bin
+#
+# go to each to-kill address... overwrite-in the value of each to-keep match
+# (note: just the first match-group, not "full match pattern"... this allows
+# one more sanity check later to verify the full-match-pattern in the new file
+# matches that of the orig file, after we only wrote the first group value.)
+#
 write-host ''
-write-host "** (Backing up $cfe to $cfe_bak.)"
-write-host "** Now using critical vals from original_cfe.bin to overwrite those in new_cfe.bin"
+write-host "** Backing up $cfe to $cfe_bak..."
 cp $cfe_new $cfe_bak
+write-host ''
+write-host "** Now using critical vals from original_cfe.bin to overwrite those in $cfe_new"
+write-host ''
 $fs = [IO.File]::OpenWrite($cfe_new) 
 $fs.seek($kill1_addr, 'Begin') | out-null
 $fs.write($keep1_valB, 0, 17) | out-null
@@ -129,7 +145,7 @@ $fs.write($keep2_valB, 0, 17) | out-null
 $fs.seek($kill3_addr, 'Begin') | out-null
 $fs.write($keep3_valB, 0, 17) | out-null
 $fs.seek($kill4_addr, 'Begin') | out-null
-$fs.write($keep4_valB, 0, 8) | out-null
+$fs.write($keep4_valB, 0, 8) | out-null # we only have 8 bytes cuz WPS code
 $fs.close()
 
 
@@ -138,11 +154,19 @@ $check2 = select-string -encoding default -path $cfe_new -pattern "0\:macaddr=$m
 $check3 = select-string -encoding default -path $cfe_new -pattern "1\:macaddr=$macpatt"
 $check4 = select-string -encoding default -path $cfe_new -pattern "secret_code=$wpspatt"
 
-$keep1.Matches.value -eq $check1.Matches.value
-$keep2.Matches.value -eq $check2.Matches.value
-$keep3.Matches.value -eq $check3.Matches.value
-$keep4.Matches.value -eq $check4.Matches.value
+if(      ($keep1.Matches.value -eq $check1.Matches.value) `
+    -and ($keep2.Matches.value -eq $check2.Matches.value) `
+    -and ($keep3.Matches.value -eq $check3.Matches.value) `
+    -and ($keep4.Matches.value -eq $check4.Matches.value) )
+{
+    write-host -foregroundcolor green "3nd (pattern-compare) sanity check passed :)"
+}else{
+    write-host -ForegroundColor red "Somehow one of the $cfe_new patterns doesn't match $cfe."
+}
 
+write-host ''
+write-host "If all 3 sanity checks passed, your $cfe_new should be ready to upload. (Hit enter to end.)"
+write-host ''
 pause
 
 
@@ -153,29 +177,3 @@ pause
 # Convert byte array to string:
 #  [System.Text.Encoding]::ASCII.GetString($bytes)
 #
-
-$old = @"
-
-#
-# now we prep for the last sanity check:
-# if we can write to the same addresses with the bytes that were 
-# originally there, we should produce a file identical to the 
-# starting file, and prove addressing is accurate
-#    new_cfe.bin.bak == unpatched_new_cfe.bin
-#
-write-host "** (Cloning $cfe_new to $cfe_unp to prep for sanity check.)"
-write-host "** Now using critical vals from (pre-mod) $cfe_new to overwrite those in $cfe_unp"
-cp $cfe_new $cfe_unp
-$fs = [IO.File]::OpenWrite($cfe_unp) 
-$fs.seek($kill1_addr, 'Begin') | out-null
-$fs.write($kill1_valB, 0, 17) | out-null
-$fs.seek($kill2_addr, 'Begin') | out-null
-$fs.write($kill2_valB, 0, 17) | out-null
-$fs.seek($kill3_addr, 'Begin') | out-null
-$fs.write($kill3_valB, 0, 17) | out-null
-$fs.close()
-
-write-host "** $cfe_unp should now be identical to $cfe_bak"
-write-host "** (demonstrating modification addresses were accurate)"
-
-"@
